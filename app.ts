27e83d8570feca34ec880035e44095111d26a3b6
Mushtaque Ahmed <mushtaque87@gmail.com>
2023-09-30 21:02:24 +0400

    log logs in access.log file

--- app.ts
+++ app.ts
@@ -4,10 +4,20 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import compression from 'compression';
 import taskRoutes from './src/routes/taskRoutes';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 
 app.use(cors());
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+  flags: 'a',
+});
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
+
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(compression());
